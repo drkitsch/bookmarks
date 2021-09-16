b99c0a1ff0c78513ac9c7e77253270393a90f32d
Harry Parsons <hparsons93@gmail.com>
2021-09-16 16:37:08 +0100

    completed step 9

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -6,15 +6,17 @@ describe Bookmark do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.come');")
       connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
 
       bookmark_list = Bookmark.all
 
       expect(bookmark_list).to include("http://www.makersacademy.com")
-      expect(bookmark_list).to include("http://www.destroyallsoftware.com")
+      expect(bookmark_list).to include("http://www.destroyallsoftware.come")
       expect(bookmark_list).to include("http://www.google.com")
     
     end 
   end 
 end 
+
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,23 @@
 require_relative './setup_test_database'
 
-ENV['RACK_ENV'] = 'test'
+#ENV['RACK_ENV'] = 'test'
 ENV['ENVIRONMENT'] = 'test'
-
-# RSpec.configure do |config| 
-#   config.before(:each) do 
-#     setup_test_database
-#   end 
-# # end
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require_relative './setup_test_database'
 
 Capybara.app = Bookmarks
+
+RSpec.configure do |config| 
+  config.before(:each) do 
+    setup_test_database
+  end 
+end 
+
 
 
 
